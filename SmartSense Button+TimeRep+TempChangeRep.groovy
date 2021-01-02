9573934b87cf134b860a759b8bdc2fe062bf447c
Mariano <74271621+Mariano-Github@users.noreply.github.com>
2021-01-03 00:11:25 +0100

    Update SmartSense Button+TimeRep+TempChangeRep.groovy

--- SmartSense Button+TimeRep+TempChangeRep.groovy
+++ SmartSense Button+TimeRep+TempChangeRep.groovy
@@ -46,7 +46,7 @@ metadata {
         }
         section {
             input "tempOffset", "number", title: "Temperature offset", description: "Select how many degrees to adjust the temperature.", range: "-100..100", displayDuringSetup: false
-            input "TempReportTimeMax", "number", title: "Temperature Report Interval", description: "Select how many minutes interval for temperature report (default 5).", range: "1..120", displayDuringSetup: true
+            input "TempReportTimeMax", "number", title: "Temperature Report Interval", description: "Select how many minutes interval for temperature report (default 5).", range: "1..240", displayDuringSetup: true
             input "TempReportTrigger", "number", title: "Temperature Report Trigger", description: "Select how many degrees change (1º=100) trigger temperature report (default 100).", range: "50..300", displayDuringSetup: true
         }
     }
