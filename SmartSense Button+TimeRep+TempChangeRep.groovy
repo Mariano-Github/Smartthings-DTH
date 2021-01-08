1a9a03389259ff2587b6529bfd6cef947adba61c
Mariano <74271621+Mariano-Github@users.noreply.github.com>
2021-01-08 11:26:02 +0100

    Update SmartSense Button+TimeRep+TempChangeRep.groovy

--- SmartSense Button+TimeRep+TempChangeRep.groovy
+++ SmartSense Button+TimeRep+TempChangeRep.groovy
@@ -293,9 +293,10 @@ def configure() {
         configCmds += zigbee.batteryConfig()
     }
     
-    if (TempReportTimeMax == null) { //For new device installation
+    if (TempReportTimeMax == null || TempReportTrigger == null) { //For new device installation
 	log.debug "Time= "+ "${TempReportTimeMax}"
 	log.debug "Trigger= "+ "${TempReportTrigger}"
+	log.debug "5 min and 1º, Applied"
         configCmds += zigbee.temperatureConfig(30, 300, 100)
     } else {
         log.debug "Time= "+ "${TempReportTimeMax}"
