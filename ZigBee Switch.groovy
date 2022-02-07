84c5bd742447e2331012537aebf2e0dd0e64cb07
Mariano <74271621+Mariano-Github@users.noreply.github.com>
2022-02-08 00:26:49 +0100

    Update ZigBee Switch.groovy

--- ZigBee Switch.groovy
+++ ZigBee Switch.groovy
@@ -29,7 +29,7 @@ metadata {
 		fingerprint profileId: "0104", inClusters: "0000, 0003, 0004, 0005, 0006, 0B05", outClusters: "0003, 0006, 0019", manufacturer: "Centralite Systems", model: "4200-C", deviceJoinName: "Centralite Outlet", ocfDeviceType: "oic.d.smartplug" //Centralite Smart Outlet
 
                 // Vimar
-		fingerprint profileId: "0104", inClusters: "0000, 0003, 0004, 0005, 0006, 0007", outClusters: "0003, 0006", manufacturer: "Vimar", model: " On_Off_Switch_v1.0", deviceJoinName: "Vimar Switch", ocfDeviceType: "oic.d.smartplug" //Vimar Switch
+		fingerprint profileId: "0104", inClusters: "0000, 0003, 0004, 0005, 0006, 0007", outClusters: "0003, 0006", manufacturer: "Vimar", model: "On_Off_Switch_v1.0", deviceJoinName: "Vimar Switch", ocfDeviceType: "oic.d.smartplug" //Vimar Switch
 
                 // eWeLink
 		fingerprint profileId: "0104", inClusters: "0000, 0003, 0004, 0005, 0006", outClusters: "0000", manufacturer: "eWeLink", model: "SA-003-Zigbee", deviceJoinName: "eWeLink Outlet", ocfDeviceType: "oic.d.smartplug" //eWeLink SmartPlug (SA-003)
